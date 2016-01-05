b44d5f3ed331f2c4bab7da8fe6a1860aab83ed08
pujie <pw.prayitno@gmail.com>
2016-01-05 16:51:55 +0700

    add stylesheet

--- sentot.js
+++ sentot.js
@@ -1,14 +1,28 @@
 $.fn.sentot = function(options){
 	var settings = $.extend({
 		dataSource:[],
+		fields:[],
 		itemPerPage:0,
 		orderBy:"name",
 		orderType:"asc",
 		maxButtonNumber:5
 	},options);
-	var obj = jQuery.parseJSON(settings.dataSource),
+	
+	var that = this,
+		obj = jQuery.parseJSON(settings.dataSource),
 		objCount = obj.length;
+		console.log("fields length",settings.fields.length);
+		for(x=0;x<settings.fields.length;x++){
+			console.log("field",settings.fields[x]);
+		}
 	for(x=0;x<obj.length;x++){
+		str='';
+		for(y=0;y<settings.fields.length;y++){
+			str+=obj[x][settings.fields[y]];
+		}
+		that.append('<div class="item">'+str+'</div>');
+
+		
 		console.log(obj[x].name,obj[x].address);
 	}
 	console.log("Total Item:",objCount);
