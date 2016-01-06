e6d66f8edb4d474739ff48381813c4e6c3729edb
pujie <pw.prayitno@gmail.com>
2016-01-06 14:58:11 +0700

    add search function

--- sentot.js
+++ sentot.js
@@ -19,9 +19,11 @@ $.fn.sentot = function(options){
 		obj = jQuery.parseJSON(settings.dataSource),
 		objCount = obj.length,
 		pageAmount=countPages(settings.itemPerPage,objCount),
-		populateItems = function(firstItem,itemPerPage){
-			if(parseInt(itemPerPage)+parseInt(firstItem)>objCount){
-				pageLastItem = objCount;
+		populateItems = function(_obj,firstItem,itemPerPage){
+			var _objCount = _obj.length;
+			$('.item').remove();
+			if(parseInt(itemPerPage)+parseInt(firstItem)>_objCount){
+				pageLastItem = _objCount;
 			}else{
 				pageLastItem = parseInt(itemPerPage)+parseInt(firstItem);
 			}
@@ -29,7 +31,7 @@ $.fn.sentot = function(options){
 				for(x=parseInt(firstItem);x<pageLastItem;x++){
 					str='';
 					for(y=0;y<settings.fields.length;y++){
-						str+='<span class="subitem" style="display:none;">'+obj[x][settings.fields[y]]+'</span>';
+						str+='<span class="subitem" style="display:none;">'+_obj[x][settings.fields[y]]+'</span>';
 					}
 					$('#container').append('<div class="item">'+str+'</div>')
 					$newItem = $('#container .item .subitem');
@@ -39,27 +41,43 @@ $.fn.sentot = function(options){
 				for(x=parseInt(firstItem);x<pageLastItem;x++){
 					str='';
 					for(y=0;y<settings.fields.length;y++){
-						str+='<span class="subitem">'+obj[x][settings.fields[y]]+'</span>';
+						str+='<span class="subitem">'+_obj[x][settings.fields[y]]+'</span>';
 					}
 					$('#container').append('<div class="item">'+str+'</div>')
 				}				
 			}
 		}
-	populateItems(0,settings.itemPerPage);
+	that.prepend('<span>Seaaaaarch</span><input id="searchText" />');
+	populateItems(obj,0,settings.itemPerPage);
 	that.append(makePagination(1,pageAmount));
+	that.on('keyup','#searchText',function(){
+		var searchResult = [];
+		$.each(obj,function(x,y){
+			for(i=0;i<settings.fields.length;i++){
+				if(y[settings.fields[i]].toLowerCase().indexOf($('#searchText').val().toLowerCase())>=0){
+					console.log('FOUND',y[settings.fields[i]]);
+					searchResult.push(y);
+					//populateItems(searchResult,0,settings.itemPerPage);
+				}
+				//console.log('items',y[settings.fields[i]]);
+			}
+		})
+		populateItems(searchResult,0,settings.itemPerPage);
+		//console.log('search text',$('#searchText').val());
+	});
 	that.on('click','.pagination',function(){
 		that.find('.pagination').removeClass('selected');
 		$(this).addClass('selected');
-		$('.item').remove();
+		//$('.item').remove();
 		switch($(this).attr('value')){
 			case 'first':
-				populateItems(0,settings.itemPerPage);
+				populateItems(obj,0,settings.itemPerPage);
 			break;
 			case 'last':
-				populateItems((pageAmount-2)*settings.itemPerPage,settings.itemPerPage);
+				populateItems(obj,(pageAmount-2)*settings.itemPerPage,settings.itemPerPage);
 			break;
 			default:
-				populateItems(parseInt(settings.itemPerPage)*parseInt($(this).attr('value')-1),settings.itemPerPage);
+				populateItems(obj,parseInt(settings.itemPerPage)*parseInt($(this).attr('value')-1),settings.itemPerPage);
 			break;
 		}
 		
