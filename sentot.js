9c023de10bd3dfa3e8d813c025de400c048ad41e
pujie <pw.prayitno@gmail.com>
2016-01-06 08:37:20 +0700

    apply pagination

--- sentot.js
+++ sentot.js
@@ -2,30 +2,41 @@ $.fn.sentot = function(options){
 	var settings = $.extend({
 		dataSource:[],
 		fields:[],
-		itemPerPage:0,
+		itemPerPage:4,
 		orderBy:"name",
 		orderType:"asc",
 		maxButtonNumber:5
 	},options);
+	$(this).append('<div id="container"></div>');
 	var that = this,
 		obj = jQuery.parseJSON(settings.dataSource),
 		objCount = obj.length,
-		pageAmount=countPages(settings.itemPerPage,objCount);
-	for(x=0;x<obj.length;x++){
-		str='';
-		for(y=0;y<settings.fields.length;y++){
-			str+='<span class="subitem">'+obj[x][settings.fields[y]]+'</span>';
+		pageAmount=countPages(settings.itemPerPage,objCount),
+		populateItems = function(firstItem,itemPerPage){
+			console.log('item populated',firstItem,itemPerPage);
+			for(x=parseInt(firstItem);x<parseInt(itemPerPage)+parseInt(firstItem);x++){
+				str='';
+				for(y=0;y<settings.fields.length;y++){
+					str+='<span class="subitem">'+obj[x][settings.fields[y]]+'</span>';
+					console.log('item got',x,obj[x][settings.fields[y]]);
+				}
+				$('#container').append('<div class="item">'+str+'</div>');
+			}	
 		}
-		that.append('<div class="item">'+str+'</div>');
-	}
+	populateItems(0,settings.itemPerPage);
 	that.append(makePagination(1,pageAmount));
+	that.on('click','.pagination',function(){
+		console.log('this class',$(this).attr('value'));
+		$('.item').remove();
+		populateItems(parseInt(settings.itemPerPage)*parseInt($(this).attr('value')-1),settings.itemPerPage);
+	});
 }
 makePagination = function(firstPage,pageAmount){
 	var str = '';
 	for(var i=firstPage;i<pageAmount;i++){
-		str+='<span class="pagination '+i+'">'+i+ '</span>';
+		str+='<span class="pagination" value="'+i+'">'+i+ '</span>';
 	}
-	return '<div><span class="pagination">Previous</span> '+str+' <span class="pagination">Next</span></div>';
+	return '<div><span class="pagination" value="prev">Previous</span> '+str+' <span class="pagination" value="Last">Last</span></div>';
 }
 countPages = function(itemPerPage,itemCount){
 	if(itemPerPage>itemCount){
